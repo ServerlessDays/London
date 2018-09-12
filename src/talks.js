bdbf83fb8d8e95827a46df5089d8c5fb969bd0f4
Alex Casalboni <alexsaxmib@gmail.com>
2018-09-12 10:40:39 +0200

    updated talks and bio

--- src/talks.js
+++ src/talks.js
@@ -1,12 +1,12 @@
 module.exports = [
   {
     key: "adam-matan",
-    bio: "",
+    bio: "Developer &amp; DevRel @ Binaris. Cloud &amp; Serverless developer. Previously Vioozer, Yahoo and Waze.",
     photoUrl: "adam-matan.jpg",
     title: "Think Functions",
     speaker: "Adam matan",
-    abstract: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, magnam quia! Amet laudantium perferendis esse assumenda atque vitae quisquam. Commodi iste consectetur dolor exercitationem blanditiis aspernatur ducimus veniam fugiat libero!
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, magnam quia! Amet laudantium perferendis esse assumenda atque vitae quisquam. Commodi iste consectetur dolor exercitationem blanditiis aspernatur ducimus veniam fugiat libero!`
+    abstract: `What would apps look like if we never had to think about servers, performance or even cost? This session demonstrates how an Uber-like ridesharing application can be built entirely from functions - starting with a architectural overview and progressing to actual JS code.
+    This session demonstrates how a real-time app can be built solely out of functions while remaining responsive and cost-effective. We present a architecture overview of the app, and walk through the process of splitting it into individual services.`
   },
   {
     key: "yan-cui",
